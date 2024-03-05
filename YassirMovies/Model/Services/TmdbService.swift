a3b69973100660b5599c5c37acd4f6652e2d2848
Pietro <pmessineo@purple-technology.com>
2024-03-05 14:31:40 +0100

    Created TmdbService layer for handle API Request Added first endpoint

---
+++ YassirMovies/Model/Services/TmdbService.swift
@@ -0,0 +1,16 @@
+//
+//  TmdbService.swift
+//  YassirMovies
+//
+//  Created by Pietro Messineo on 05/03/24.
+//
+
+import Foundation
+
+struct TmdbService: HTTPClient {
+    func getEditorsChoice() async throws -> EditorsChoiceResponse {
+        return try await request(
+            endpoint: TmdbEndpoint.editorsChoice, responseModel: EditorsChoiceResponse.self, decoder: .customDateDecoder
+        )
+    }
+}
