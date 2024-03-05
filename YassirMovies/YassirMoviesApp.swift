1f4120bbcca3585ef3a906206cff5f87a57fa4c4
Pietro <pmessineo@purple-technology.com>
2024-03-05 14:13:47 +0100

    Initial Commit

---
+++ YassirMovies/YassirMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YassirMoviesApp.swift
+//  YassirMovies
+//
+//  Created by Pietro Messineo on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct YassirMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
