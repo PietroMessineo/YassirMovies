89b283213b91bf752ff8dbf00cb051614aba966f
Pietro <pmessineo@purple-technology.com>
2024-03-06 14:37:44 +0100

    Added comments to TmdbManager

--- YassirMovies/Model/Managers/TmdbManager.swift
+++ YassirMovies/Model/Managers/TmdbManager.swift
@@ -9,19 +9,23 @@ import Foundation
 
 @MainActor
 final class TmdbManager: ObservableObject {
+    
+    // Service protocol to abstract TMDB service details
     let service: TmdbServiceProtocol
     
-    @Published var isLoading = false
-    @Published var movieEditorsChoice: [Items]?
-    @Published var movieDetails: MovieDetailsResponse?
-    @Published var movieDiscoverList: [Items] = []
-    @Published var searchResults: [SearchResult] = []
-    @Published var movieWatchProviders: [String: CountryResult] = [:]
+    // Properties to track and update UI states and data
+    @Published var isLoading = false // Indicates network activity
+    @Published var movieEditorsChoice: [Items]? // Stores editor's choice movie list
+    @Published var movieDetails: MovieDetailsResponse? // Stores details for a specific movie
+    @Published var movieDiscoverList: [Items] = [] // Stores list of movies fetched from "Discover" API
+    @Published var searchResults: [SearchResult] = [] // Stores results from a search query
+    @Published var movieWatchProviders: [String: CountryResult] = [:] // Key-Value pairs of movie IDs and their available watch providers by country
     
     init(service: TmdbServiceProtocol = TmdbService()) {
         self.service = service
     }
     
+    // Fetches and updates the editor's choice movies
     func getMoviesEditorsChoice() async throws {
         isLoading = true
         let editorsChoiceResponse = try await service.getEditorsChoice()
@@ -31,6 +35,7 @@ final class TmdbManager: ObservableObject {
         isLoading = false
     }
     
+    // Fetches and updates the details for a single movie based on its ID
     func getMovieDetails(id: String) async throws {
         isLoading = true
         let movieDetail = try await service.getMovieDetails(id: id)
@@ -38,28 +43,32 @@ final class TmdbManager: ObservableObject {
         isLoading = false
     }
     
+    // Fetches and appends movies from the Discover API to the existing discover list, supporting pagination
     func getMovieDiscover(page: Int, movieList: DiscoverMovieList) async throws {
         isLoading = true
         let discover = try await service.getDiscover(page: page, movieList: movieList)
         if let items = discover.results {
-            movieDiscoverList.append(contentsOf: items)
+            movieDiscoverList.append(contentsOf: items) // Append new discoveries for infinite scrolling
         }
         isLoading = false
     }
     
+    // Executes a search query and filters out results without images, updates searchResults
     func searchFor(query: String) async throws {
         isLoading = true
         let searchResponse = try await service.searchFor(query: query)
         if let results = searchResponse.results {
+            // Store only results with images to ensure better UI
             self.searchResults = results.filter({ $0.poster_path != nil || $0.profile_path != nil })
         }
         isLoading = false
     }
     
+    // Fetches available watch providers for a specific movie by its ID, useful for region-specific viewing options
     func getMovieWatchProviders(movieId: String) async throws {
         isLoading = true
         let movieWatchProviders = try await service.getMovieWatchProviders(movieId: movieId)
-        self.movieWatchProviders = movieWatchProviders.results
+        self.movieWatchProviders = movieWatchProviders.results // Maps movie ID to available watch providers
         isLoading = false
     }
 }
