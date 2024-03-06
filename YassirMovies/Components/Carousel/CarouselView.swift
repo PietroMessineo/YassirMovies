28d7e085a550cf073d8bfd8695527cca7617730a
Pietro <pmessineo@purple-technology.com>
2024-03-06 15:00:19 +0100

    Added comments to carousel

--- YassirMovies/Components/Carousel/CarouselView.swift
+++ YassirMovies/Components/Carousel/CarouselView.swift
@@ -7,34 +7,42 @@
 
 import SwiftUI
 
+// A generic carousel view that can display any kind of view (Content) for a given collection of items (Item).
 struct CarouselView<Content: View, Item: RandomAccessCollection>: View where Item.Element: Identifiable {
-    var spacing: CGFloat = 0
-    var items: Item
-    var content: (Item.Element) -> Content
+    var spacing: CGFloat = 0 // Spacing between individual items in the carousel.
+    var items: Item // The collection of items that the carousel will display.
+    var content: (Item.Element) -> Content // A closure that takes an item from the collection and returns a view for that item.
+    
+    // The body of the CarouselView, responsible for laying out the content.
     var body: some View {
-        GeometryReader {
-            let size = $0.size
+        GeometryReader { geometryProxy in
+            let size = geometryProxy.size
+            // ScrollViewReader and ScrollView nested are needed in order to automatically scroll to the second item automatically
             ScrollViewReader { scrollViewProxy in
                 ScrollView(.horizontal) {
                     LazyHStack(spacing: 0) {
                         ForEach(items) { item in
                             content(item)
                                 .frame(width: getCardWidth())
+                                // Conditional padding to remove trailing space on the last item.
                                 .padding(.trailing, item.id == items.last?.id ? 0 : spacing)
                         }
                     }
+                    // Center the first item initially by adding equal padding on both sides.
                     .padding(.horizontal, (size.width - getCardWidth()) / 2)
                     .onAppear {
+                        // Auto-scroll to the second item
                         if let secondItemId = items.dropFirst().first?.id {
                             scrollViewProxy.scrollTo(secondItemId, anchor: .center)
                         }
                     }
                 }
-                .scrollIndicators(.hidden)
+                .scrollIndicators(.hidden) // Hide the scroll bar indicators for a cleaner look.
             }
         }
     }
 
+    // Determine the width of the cards based on screen width to maintain a consistent aspect ratio.
     func getCardWidth() -> CGFloat {
         let screenWidth = UIScreen.main.bounds.width
         let widthRatio: CGFloat = 290 / 430
@@ -45,7 +53,7 @@ struct CarouselView<Content: View, Item: RandomAccessCollection>: View where Ite
 
 struct CoverFlowItem: Identifiable {
     let id: UUID = .init()
-    var item: Items
+    var item: Items 
 }
 
 #Preview {
--- YassirMovies/Components/Image Loader/RemoteImageView.swift
+++ YassirMovies/Components/Image Loader/RemoteImageView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 import Kingfisher
 
+// Wrapper that can be reused to pull images from TMDB database
 struct RemoteImageView: View {
     let imagePath: String?
     
     var body: some View {
+        // Check if image is not nil, if not append to base path and return imageView
         if let imagePath = imagePath, let url = URL(string: "https://image.tmdb.org/t/p/w500" + imagePath) {
             let image = KFImage.url(url)
                 .placeholder({ progress in
